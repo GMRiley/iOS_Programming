e1d8c394a73e8ace9443107eea4330cb7a79bcad
Riley <001417108@N358-D5.local>
2020-02-19 22:05:26 -0500

    homepwner assignment

---
+++ Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Riley, Kyle M on 2/19/20.
+//  Copyright © 2020 Riley, Kyle M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.let itemStore = ItemStore()
+        
+    }
+
+
+}
+
